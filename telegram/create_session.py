d69f81df5ad7089c189cc7b27f5134925f0ec4ef
Danyla <danyla.kpk@gmail.com>
2025-01-23 23:37:58 +0300

    Without async

--- telegram/create_session.py
+++ telegram/create_session.py
@@ -1,33 +1,39 @@
 import time
-
+import asyncio
 from pyrogram import Client
 from config import settings
 from utils.scraper import Scraper
 from utils.data_manager import Data_Manager
+from loguru import logger as ll
 import time
 import os
 
+import asyncio
+from pyrogram import Client
+from config import settings
+from utils.scraper import Scraper
+from utils.data_manager import Data_Manager
+from loguru import logger as ll
+import os
 
-def create_session():
+
+async def create_session():
     acc = Scraper()
     acc.get_account_session()
     data = Data_Manager()
-    time.sleep(2)
+
+    await asyncio.sleep(3)
     print("Starting Pyrogram client...")
 
-    SESSION_NAME = input("Please enter your session name: ")
+    # Asynchronously get input from the user
+    SESSION_NAME = await asyncio.to_thread(input, "Please enter your session name: ")
     SESSION_NAME_FILE = SESSION_NAME + ".session"
+    all_files_and_dirs = os.listdir("telegram/sessions")
+    files = [f for f in all_files_and_dirs if os.path.isfile(os.path.join("telegram/sessions", f))]
 
-    folder_path = "telegram/sessions"
-
-    all_files_and_dirs = os.listdir(folder_path)
-
-    # Фильтруем только файлы
-    files = [f for f in all_files_and_dirs if os.path.isfile(os.path.join(folder_path, f))]
     if SESSION_NAME_FILE in list(files):
-        print(f"{SESSION_NAME_FILE} already exist")
+        print(f"{SESSION_NAME_FILE} already exists.")
         return False
-
 
     PROXY = {
         "scheme": "socks5",
@@ -36,24 +42,22 @@ def create_session():
         "username": acc.proxy_username,
         "password": acc.proxy_password
     }
-    app = Client(SESSION_NAME, api_id=settings.API_ID, api_hash=settings.API_HASH, proxy=PROXY, workdir="telegram/sessions/")
 
-    # Start the client
-    with app:
-        try:
-            if not app.is_connected:
-                app.connect()
+    app = Client(SESSION_NAME, api_id=settings.API_ID, api_hash=settings.API_HASH, proxy=PROXY,
+                 workdir="telegram/sessions/")
 
+    try:
+        async with app:
+            if not await app.is_connected():
+                await app.connect()
             print("Connected successfully!")
-
-            # Check if user is already authorized
-            me = app.get_me()
+            me = await app.get_me()
             if me:
                 print(f"Logged in as {me.first_name} ({me.username})")
-                status = data.add_new_tg_session(session_name=SESSION_NAME, session_pass='Gennady', session_proxy=acc.proxy)
-                print(status)
+                status = data.add_new_tg_session(session_name=SESSION_NAME, session_pass='Gennady',
+                                                 session_proxy=acc.proxy)
             else:
-                print('Cant loggin')
+                print('Can\'t log in.')
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
-        except Exception as e:
-            print(f"An error occurred: {e}")
